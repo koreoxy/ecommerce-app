d43e4f44ccec606b1214ce19d8610a8369ffcdc4
koreoxy <rayssankn@gmail.com>
2022-12-14 13:33:44 +0700

    update list product

---
+++ utils/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: 'Sayur Sawi',
+      slug: 'sayur-sawi',
+      category: 'Sayur',
+      image: '/images/sawi.jpg',
+      price: 10,
+      rating: 4.5,
+      numRevies: 8,
+      countInStock: 20,
+      description: 'Sayur sawi segar',
+    },
+    {
+      name: 'Kacang Panjang',
+      slug: 'kacang-panjang',
+      category: 'Sayur',
+      image: '/images/kacangpanjang.jpg',
+      price: 20,
+      rating: 3.5,
+      numRevies: 8,
+      countInStock: 20,
+      description: 'Kacang panjan kualitas bagus',
+    },
+    {
+      name: 'Buncis',
+      slug: 'buncis',
+      category: 'Sayur',
+      image: '/images/buncis.jpg',
+      price: 10,
+      rating: 5.5,
+      numRevies: 8,
+      countInStock: 50,
+      description: 'Buncis segar',
+    },
+    {
+      name: 'Pakcoy',
+      slug: 'pakcoy',
+      category: 'Sayur',
+      image: '/images/pakcoy.jpg',
+      price: 10,
+      rating: 5.5,
+      numRevies: 8,
+      countInStock: 50,
+      description: 'Sayur Pakcoy segar',
+    },
+  ],
+};
+
+export default data;
