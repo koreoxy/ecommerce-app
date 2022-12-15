33a5243b5ffcb9262fac1395c332409cebecdeca
koreoxy <rayssankn@gmail.com>
2022-12-15 13:29:09 +0700

    update halaman shopping cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,7 +8,7 @@ import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store);
-
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
@@ -24,6 +24,7 @@ export default function ProductScreen() {
       return;
     }
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
   };
 
   return (
