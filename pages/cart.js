1b1d58dac90aaef1a66023a35a2e1eda7fd728bf
koreoxy <rayssankn@gmail.com>
2022-12-15 22:06:10 +0700

    update save cart

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,9 @@ import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import { XCircleIcon } from '@heroicons/react/24/outline';
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 
-export default function CartScreen() {
+function CartScreen() {
   const { state, dispatch } = useContext(Store);
   const router = useRouter();
   const {
@@ -105,3 +106,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
