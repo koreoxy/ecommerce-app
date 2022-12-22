0bdeb09cddbed71f769de616f80bf45368dad85e
koreoxy <rayssankn@gmail.com>
2022-12-22 15:32:21 +0700

    update user profile page

---
+++ pages/api/auth/update.js
@@ -0,0 +1,45 @@
+import { getSession } from 'next-auth/react';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+import bcryptjs from 'bcryptjs';
+
+async function handler(req, res) {
+  if (req.method !== 'PUT') {
+    return res.status(400).send({ message: `${req.method} not supported` });
+  }
+
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).send({ message: 'signin required' });
+  }
+
+  const { user } = session;
+  const { name, email, password } = req.body;
+
+  if (
+    !name ||
+    !email ||
+    !email.includes('@') ||
+    (password && password.trim().length < 5)
+  ) {
+    res.status(422).json({
+      message: 'Validate error',
+    });
+    return;
+  }
+
+  await db.connect();
+  const toUpdateUser = await User.findById(user._id);
+  toUpdateUser.name = name;
+  toUpdateUser.email = email;
+  if (password) {
+    toUpdateUser.password = bcryptjs.hashSync(password);
+  }
+  await toUpdateUser.save();
+  await db.disconnect();
+  res.send({
+    message: 'User updated',
+  });
+}
+
+export default handler;
