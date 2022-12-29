c047e62c99eec78f7d75ffec21e570adda8ec232
koreoxy <rayssankn@gmail.com>
2022-12-29 15:16:33 +0700

    update function delete and created product

--- pages/api/admin/products/index.js
+++ pages/api/admin/products/index.js
@@ -7,12 +7,33 @@ const handler = async (req, res) => {
   if (!session || !session.user.isAdmin) {
     return res.status(401).send('admin signin required');
   }
-  // const { user } = session;
+  //const { user } = session;
   if (req.method === 'GET') {
     return getHandler(req, res);
+  } else if (req.method === 'POST') {
+    return postHandler(req, res);
   } else {
     return res.status(400).send({ message: 'Method not allowed' });
   }
+};
+
+const postHandler = async (req, res) => {
+  await db.connect();
+  const newProduct = new Product({
+    name: 'sample name',
+    slug: 'sample-name-' + Math.random(),
+    image: '/images/shirt1.jpg',
+    price: 0,
+    category: 'sample category',
+    countInStock: 0,
+    description: 'sample description',
+    rating: 0,
+    numReviews: 0,
+  });
+
+  const product = await newProduct.save();
+  await db.disconnect();
+  res.send({ message: 'Product created successfully', product });
 };
 
 const getHandler = async (req, res) => {
